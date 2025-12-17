adbdfa79391dd15d9a51b2303772287c609309a0
Sebastijan Grabar <8758580+baso53@users.noreply.github.com>
2025-12-17 14:15:39 +0100

    Initial Commit

---
+++ McrawMetadataAnalyzer/McrawMetadataAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  McrawMetadataAnalyzerApp.swift
+//  McrawMetadataAnalyzer
+//
+//  Created by Sebastijan on 17.12.2025..
+//
+
+import SwiftUI
+
+@main
+struct McrawMetadataAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
